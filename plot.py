597e011b02459f0f95b9fccb357931858bd1b657
Mirjam <mirjam.vandermheen@research.uwa.edu.au>
2020-12-01 12:10:45 +0800

    Minor changes and additions to plotting functions

--- plot.py
+++ plot.py
@@ -7,8 +7,11 @@ import cartopy.mpl.ticker as cticker
 import numpy as np
 import cmocean.cm as cmo
 
-def _cartopy_map_and_grid(ax,lon_range=[-180,180],lat_range=[-80,90],dx=60,dy=20):
-    ax.add_feature(cftr.LAND,facecolor='#A3A3A3',edgecolor='k',zorder=2)
+def _cartopy_map_and_grid(ax,lon_range=[-180,180],lat_range=[-80,90],dx=60,dy=20,resolution=None):
+    if resolution is None:
+        ax.add_feature(cftr.LAND,facecolor='#A3A3A3',edgecolor='k',zorder=2)
+    else:
+        ax.coastlines(resolution=resolution, color='k', linewidth=1, zorder=2)
     ax.set_extent([lon_range[0],lon_range[1],lat_range[0],lat_range[1]],ccrs.PlateCarree())
     lon_formatter = cticker.LongitudeFormatter()
     lat_formatter = cticker.LatitudeFormatter()
@@ -22,8 +25,8 @@ def _cartopy_map_and_grid(ax,lon_range=[-180,180],lat_range=[-80,90],dx=60,dy=20
     ax.yaxis.set_major_formatter(lat_formatter)    
     ax.grid(b=True,linewidth=2,color='k',linestyle='-')
 
-def _pcolormesh_and_colorbar(fig,ax,lon,lat,variable_values,cbar_label,cmap='viridis'):
-    c = ax.pcolormesh(lon,lat,variable_values,cmap=cmap,transform=ccrs.PlateCarree(),zorder=1)
+def _pcolormesh_and_colorbar(fig,ax,lon,lat,variable_values,cbar_label,vmin=None,vmax=None,cmap='viridis'):
+    c = ax.pcolormesh(lon,lat,variable_values,cmap=cmap,transform=ccrs.PlateCarree(),zorder=1,vmin=vmin,vmax=vmax)
     pos = ax.get_position()
     cbax = fig.add_axes([pos.x0+pos.width+0.01,pos.y0,0.02,pos.height])
     cbar = fig.colorbar(c,label=cbar_label,cax=cbax)
@@ -41,6 +44,28 @@ def _quiverplot(ax,lon,lat,u,v,thin=5,scale=25):
 def _add_text_watermark(ax,wm_text='MegaMove',color='#EEEEEE',fontsize=130,alpha=0.5):
     ax.text(0.5,0.5,wm_text,fontsize=fontsize,color=color,
              ha='center',va='center',alpha=alpha)
+
+def map_wa_velocities(modeldata,cbar_label='Surface currents [m/s]',thin=5,scale=25,
+                      cmap=cmo.speed,output_path=None):
+    lon = modeldata.lon.values
+    lat = modeldata.lat.values
+    u = np.copy(modeldata.u.values[0,:,:])
+    v = np.copy(modeldata.v.values[0,:,:])
+    vel = np.copy(modeldata.vel.values[0,:,:])
+    u[u==0] = np.nan
+    v[v==0] = np.nan
+    vel[vel==0] = np.nan
+    lon_range = [112,118]
+    lat_range = [-36,-25]
+    plt.style.use('input/plots.mplstyle')
+    fig = plt.figure()
+    ax = plt.gca(projection=ccrs.PlateCarree())
+    _cartopy_map_and_grid(ax,lon_range=lon_range,lat_range=lat_range,dx=2,dy=2,resolution='50m')
+    _pcolormesh_and_colorbar(fig,ax,lon,lat,vel,cbar_label,cmap=cmap,vmin=0,vmax=1.5)
+    _quiverplot(ax,lon,lat,u,v,thin=thin,scale=scale)
+    if output_path is not None:
+        plt.savefig(output_path,bbox_inches='tight',dpi=300)
+    plt.show()
 
 def global_map_velocities(lon,lat,u,v,vel,cbar_label,thin=5,scale=25,
                           cmap='viridis',output_path=None):
